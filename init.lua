e553e19be1c2414c4fe9ef6b9890cdb11abe5bfd
kuzda <dmrkuznetsov@gmail.com>
2025-05-31 16:47:31 +0700

    Add C# support(LSP)

--- init.lua
+++ init.lua
@@ -689,6 +689,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        omnisharp = {},
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
